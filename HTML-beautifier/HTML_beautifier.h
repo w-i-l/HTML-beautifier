501dfa9aa8cce52fc10514f517a4bd18e3addba6
Mihai Ocnaru <mihai.ocnaru@mready.net>
2024-09-17 11:21:53 +0300

    first commit

---
+++ HTML-beautifier/HTML_beautifier.h
@@ -0,0 +1,18 @@
+//
+//  HTML_beautifier.h
+//  HTML-beautifier
+//
+//  Created by Mihai Ocnaru on 17.09.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HTML_beautifier.
+FOUNDATION_EXPORT double HTML_beautifierVersionNumber;
+
+//! Project version string for HTML_beautifier.
+FOUNDATION_EXPORT const unsigned char HTML_beautifierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTML_beautifier/PublicHeader.h>
+
+
